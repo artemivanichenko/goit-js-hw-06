5a6a5b873cd3c11a823f8731e66926b6b8fcf3c6
Artem <ivanichenko.ua@gmail.com>
2023-02-17 13:17:57 +0200

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,15 @@
 const listItems = document.querySelector("#categories");
 console.log(`Number of categories: ${listItems.children.length}`);
 console.log("");
+
+const itemsEl = document.querySelectorAll(".item");
+
+itemsEl.forEach((el) => {
+  console.log(`Category: ${el.firstElementChild.textContent};`);
+  console.log(`Elements: ${el.lastElementChild.children.length};`);
+  console.log("");
+});
+
 // const itemsEl = document.querySelectorAll(".item");
 // const itemsEllist = document.querySelectorAll("ul");
 // console.log(`Category: ${itemsEl[0].firstElementChild.textContent};`);
@@ -11,12 +20,3 @@ console.log("");
 // console.log("");
 // console.log(`Category: ${itemsEl[2].firstElementChild.textContent};`);
 // console.log(`Elements: ${itemsEllist[3].children.length};`);
-
-const itemsEl = document.querySelectorAll(".item");
-
-itemsEl.forEach((item) => {
-  const itemsEllist = item.querySelectorAll("li");
-  console.log(`Category: ${item.firstElementChild.textContent};`);
-  console.log(`Elements: ${itemsEllist.length};`);
-  console.log("");
-});
